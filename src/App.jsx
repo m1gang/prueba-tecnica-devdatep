be4aca4d0fd1b4c82ce5cea9e9df3776206e7e8b
MiGaNg <miguelangelyv1@gmail.com>
2026-03-02 10:56:17 -0500

    Cambios no guardados

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './App.css'
-import { HomePage } from './pages/HomePage';
-import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router';
-import { CharacterPage } from './pages/CharacterPage';
-import { SearchPage } from './pages/SearchPage';
+import { RouterProvider } from 'react-router';
 import { appRouter } from './router/app.routes';
 import { Toaster } from 'sileo';
 
